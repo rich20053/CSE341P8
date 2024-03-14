562d73357ab70303314de869f1d21a521961f9ae
Mark Richmond <77017509+rich20053@users.noreply.github.com>
2024-03-13 21:51:13 -0600

    Reset + 1

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
+const dotenv = require('dotenv');
 const express = require('express');
 const mongodb = require('./models/connect');
 const bodyParser = require('body-parser');
+const http = require('http');
+const logger = require('morgan');
+const path = require('path');
+const router = require('./routes/index');
+const { auth } = require('express-openid-connect');
 
-const port = process.env.PORT || 8080;
+//dotenv.load();
+
 const app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 app
+  .use(logger('dev'))
+  .use(express.static(path.join(__dirname, 'public')))
   .use(bodyParser.json())
   .use('/', require('./routes'))
   .use(express.urlencoded({ extended: true }))
@@ -14,12 +26,52 @@ app
     next();
   })
 
+  const config = {
+    authRequired: false,
+    auth0Logout: true
+  };
+
+  const port = process.env.PORT || 8080;
+  if (!config.baseURL && !process.env.BASE_URL && process.env.PORT && process.env.NODE_ENV !== 'production') {
+    config.baseURL = `http://localhost:${port}`;
+  }
+  
+  app.use(auth(config));
+
+  // Middleware to make the `user` object available for all views
+  app.use(function (req, res, next) {
+    res.locals.user = req.oidc.user;
+    next();
+  });
+
+  app.use('/', router);
+
+// Catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// Error handlers
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: process.env.NODE_ENV !== 'production' ? err : {}
+  });
+});
+
+
+  
 mongodb.initDb((err, mongodb) => {
   if (err) {
     console.log(err);
   } else {
-    app.listen(port);
-    console.log(`Connected to DB and listening on ${port}`);
+    http.createServer(app)
+    .listen(port, () => {
+      console.log(`Listening on ${config.baseURL}`);
+    });
   }
 });
 
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const { requiresAuth } = require('express-openid-connect');
+
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Auth0 Webapp sample Nodejs',
+    isAuthenticated: req.oidc.isAuthenticated()
+  });
+});
+
+router.get('/profile', requiresAuth(), function (req, res, next) {
+  res.render('profile', {
+    userProfile: JSON.stringify(req.oidc.user, null, 2),
+    title: 'Profile page'
+  });
+});
 
 router.use('/', require('./swagger'));
 router.use('/songs', require('./songs'));
